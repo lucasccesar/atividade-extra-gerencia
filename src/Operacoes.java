f291f8cccf42694fc57edd2dfb994326b2d85145
Glauco Siqueira <glauco0909@gmail.com>
2025-09-08 11:23:19 -0300

    Classe operacoes e tag v1.0

---
+++ src/Operacoes.java
@@ -0,0 +1,19 @@
+public class Operacoes {
+
+    public static double soma(double numero1, double numero2) {
+        return numero1 + numero2;
+    }
+
+    public static double subtracao(double numero1, double numero2) {
+        return numero1 - numero2;
+    }
+
+    public static double multiplicacao(double numero1, double numero2) {
+        return numero2 * numero1;
+    }
+
+    public static double divisao(double numero1, double numero2) {
+        return numero1 / numero2;
+    }
+
+}
