bed1f67f9873705ea7f516d7e066eaeb38d5cd0b
lucas (lab novo) <lucasccesar05@gmail.com>
2025-09-08 11:43:31 -0300

    fixed main class bug and updated README.md

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,54 @@
 import java.util.Scanner;
 
-public static void main(String[] args) {
+public class Main{
 
-    Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
 
-    System.out.println("Qual operação deseja executar?");
-    System.out.println("1- Soma");
-    System.out.println("2- Subtração");
-    System.out.println("3- Multiplicação");
-    System.out.println("4- Divisão");
+        Scanner scan = new Scanner(System.in);
 
-    int operacao = scan.nextInt();
+        System.out.println("Qual operação deseja executar?");
+        System.out.println("1- Soma");
+        System.out.println("2- Subtração");
+        System.out.println("3- Multiplicação");
+        System.out.println("4- Divisão");
 
-    while (operacao < 0 || operacao > 4) {
-        System.out.println("Digite uma operação válida!");
+        int operacao = scan.nextInt();
+        String op;
+
+        while (operacao < 0 || operacao > 4) {
+            System.out.println("Digite uma operação válida!");
+        }
+
+        System.out.print("Digite um numero: ");
+        double numero1 = scan.nextDouble();
+        System.out.print("Digite outro numero: ");
+        double numero2 = scan.nextDouble();
+        double resultado;
+
+        switch (operacao) {
+            case 1:
+                op = "+";
+                resultado = Operacoes.soma(numero1, numero2);
+                break;
+            case 2:
+                op = "-";
+                resultado = Operacoes.subtracao(numero1, numero2);
+                break;
+            case 3:
+                op = "*";
+                resultado = Operacoes.multiplicacao(numero1, numero2);
+                break;
+            case 4:
+                op = "/";
+                resultado = Operacoes.divisao(numero1, numero2);
+                break;
+            default:
+                op = "";
+                resultado = 0;
+        }
+
+        System.out.println("Resultado de " + numero1 + " " + op + " " + numero2 + " = " + resultado);
     }
 
-    System.out.print("Digite um numero: ");
-    double numero1 = scan.nextDouble();
-    System.out.print("Digite outro numero: ");
-    double numero2 = scan.nextDouble();
-    double resultado;
-
-    switch (operacao) {
-        case 1:
-            resultado = Operacoes.soma(numero1, numero2);
-            break;
-        case 2:
-            resultado = Operacoes.subtracao(numero1, numero2);
-            break;
-        case 3:
-            resultado = Operacoes.multiplicacao(numero1, numero2);
-            break;
-        case 4:
-            resultado = Operacoes.divisao(numero1, numero2);
-            break;
-        default:
-            resultado = 0;
-    }
-
-    System.out.println("Resultado de " + numero1+ " " + numero2 + " : " + resultado);
 }
+
