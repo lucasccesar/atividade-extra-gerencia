dd55315dbf3337575feb061f18406793eaffffc1
lucas (lab novo) <lucasccesar05@gmail.com>
2025-09-08 11:26:31 -0300

    added user inputs functionalities

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+
+public static void main(String[] args) {
+
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Qual operação deseja executar?");
+    System.out.println("1- Soma");
+    System.out.println("2- Subtração");
+    System.out.println("3- Multiplicação");
+    System.out.println("4- Divisão");
+
+    int operacao = scan.nextInt();
+
+    while (operacao < 0 || operacao > 4) {
+        System.out.println("Digite uma operação válida!");
     }
+
+    System.out.print("Digite um numero: ");
+    double numero1 = scan.nextDouble();
+    System.out.print("Digite outro numero: ");
+    double numero2 = scan.nextDouble();
+    double resultado;
+
+    switch (operacao) {
+        case 1:
+            resultado = Operacoes.soma(numero1, numero2);
+            break;
+        case 2:
+            resultado = Operacoes.subtracao(numero1, numero2);
+            break;
+        case 3:
+            resultado = Operacoes.multiplicacao(numero1, numero2);
+            break;
+        case 4:
+            resultado = Operacoes.divisao(numero1, numero2);
+            break;
+        default:
+            resultado = 0;
+    }
+
+    System.out.println("Resultado de " + numero1+ " " + numero2 + " : " + resultado);
 }
